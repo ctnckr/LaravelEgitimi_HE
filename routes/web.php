5c5be97c61aba19244b77e27d2e2e0f3e842ce35
ctnckr <cetinceker@arkingroup.com>
2023-04-18 22:14:23 +0300

    Bu dersin yarısındayım: 3. Laravel Routing (Link Yapısı) Nasıl Yapılır? | Route Name Verme | Route Prefix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return "asjgdlasgfalsgflasflas";
+    return view('welcome');
 });
+
+Route::controller(TestController::class)->group(function (){
+    Route::get('/test', 'test');
+});
+
